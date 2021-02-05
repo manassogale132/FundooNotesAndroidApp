5f96a19bb2a8c270f83ca8a929f03ced1ec6fe49
manassogale132 <manassogale@gmail.com>
2021-02-05 11:31:45 +0530

    Added : 'Show Password' checkbox to hide and show password in edit-text.

--- app/src/main/java/com/example/myfirstapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/LoginActivity.kt
@@ -1,21 +1,40 @@
 package com.example.myfirstapplication
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-            registerUser.setOnClickListener {
-                val intent = Intent(this,RegistrationActivity::class.java)
-                Toast.makeText(this,"Register Page Opened!", Toast.LENGTH_SHORT).show()
-                startActivity(intent)
-                finish()
+        registerUser.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            Toast.makeText(this, "Register Page Opened!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
+//---------------------------------------------------------------------------------------------------------------------------------------
+        showPassword.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            } else {
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
             }
         }
+//---------------------------------------------------------------------------------------------------------------------------------------
+    }
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/myfirstapplication/RegistrationActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/RegistrationActivity.kt
@@ -3,8 +3,13 @@ package com.example.myfirstapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.activity_registration.password
+import kotlinx.android.synthetic.main.activity_registration.showPassword
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +17,19 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
 
         alreadyUser.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
-            Toast.makeText(this,"Login Page Opened!",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            Toast.makeText(this, "Login Page Opened!", Toast.LENGTH_SHORT).show()
             startActivity(intent)
             finish()
         }
+//---------------------------------------------------------------------------------------------------------------------------------------
+        showPassword.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            } else {
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        }
+//---------------------------------------------------------------------------------------------------------------------------------------
     }
 }
