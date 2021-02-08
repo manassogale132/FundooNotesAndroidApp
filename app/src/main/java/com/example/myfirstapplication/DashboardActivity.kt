845cdd391f1e37be54893692a08c3a2bc1a0178c
manassogale132 <manassogale@gmail.com>
2021-02-08 22:40:01 +0530

    Added : On dashboard activity added navigation drawer with custom menu,header & toolbar.

--- app/src/main/java/com/example/myfirstapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/DashboardActivity.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
-class DashboardActivity  : AppCompatActivity()  {
+class DashboardActivity : AppCompatActivity() {
 
-    private lateinit var  auth: FirebaseAuth
+    private lateinit var drawer: DrawerLayout
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,23 +27,24 @@ class DashboardActivity  : AppCompatActivity()  {
         auth = FirebaseAuth.getInstance()
         val currentUser = auth.currentUser
 
-        displayUserInformation(currentUser)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        logOutbutton.setOnClickListener {
-            logoutMethod()
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    override fun onBackPressed() {                       //this will close the drawer if its open without directly closing app
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
-    //-------------------------------------------------------------------------------------------------------------------------
-    private fun displayUserInformation(currentUser: FirebaseUser?) {
-        emailTextView.text = currentUser?.email
-        nameTextView.text = currentUser?.displayName
-    }
-    //-------------------------------------------------------------------------------------------------------------------------
-    fun logoutMethod() {
-        auth.signOut();
-        Toast.makeText(baseContext, "Logged out", Toast.LENGTH_SHORT).show()
-        startActivity(Intent(this,LoginActivity::class.java))
-        finish()
-    }
-    //-------------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------
 }
