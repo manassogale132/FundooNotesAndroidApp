eb5a86aae89bdfb5251de1cf516a85f3bd89958b
manassogale132 <manassogale@gmail.com>
2021-02-09 17:59:58 +0530

    Added : 'Glide' library to get the profile image of users from there email.

--- app/src/main/java/com/example/myfirstapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/DashboardActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -12,6 +13,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.widget.ToolbarWidgetWrapper
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -78,9 +80,14 @@ class DashboardActivity : AppCompatActivity() , NavigationView.OnNavigationItemS
         val headerView : View = navigationView.getHeaderView(0)
         var headerName : TextView = headerView.findViewById(R.id.navHeaderName)
         var headerEmail : TextView = headerView.findViewById(R.id.navHeaderEmail)
+        var headerPhoto : ImageView = headerView.findViewById(R.id.navHeaderPhoto)
 
          headerName.text = currentUser?.displayName
          headerEmail.text = currentUser?.email
+
+        if (currentUser != null) {
+            Glide.with(this).load(currentUser.photoUrl).into(headerPhoto)
+        }
     }
     //-----------------------------------------------------------------------------------------------------------------
     private fun customToolBarFunction(){
